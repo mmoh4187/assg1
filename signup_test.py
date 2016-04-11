cf64ca2b9510bb4ac91ea9f09f015949a9d11d5c
mmoh4187 <mmoh4187@uni.sydney.edu.au>
2016-04-11 13:54:57 +0300

    Update signup_test.py

--- signup_test.py
+++ signup_test.py
@@ -7,22 +7,22 @@ class UserSignupTestCase(unittest.TestCase):
         sign_up.app.config['TESTING'] = True
         self.app = sign_up.app.test_client()
 
-    def signUp(self, username, email):
+    def signUp(self, username, email, password):
         return self.app.post('/signUp', data=dict(
             username = username,
-            email = email
+            email = email, password = password
         ),follow_redirects=True)
 
     def test_signUp_ok(self):
-        rv = self.signUp("ray", "ray@gmail.com")
+        rv = self.signUp("ray", "ray@gmail.com", "123456")
         assert b'success'
 
     def test_signUp_not_ok(self):
-        rv = self.signUp("admin", "admin@admin.com")
+        rv = self.signUp("admin", "admin@admin.com", "123456")
         assert b'fail'
 	
     def test_signUp_not_ok1(self):
-        rv = self.signUp("ray", "admin@admin.com")
+        rv = self.signUp("ray", "admin@admin.com", "123456")
         assert b'fail'
 
 if __name__ == '__main__':
